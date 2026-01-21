85512d8120191a9a3f2fcb8adc50f7b7c2d4aa26
Makis Laskaris <makis@laskarism.org>
2026-01-21 16:15:07 +0200

    Change to built in config parser

--- src/main/java/org/example/makispl/makispl/Makispl.java
+++ src/main/java/org/example/makispl/makispl/Makispl.java
@@ -1,113 +1,43 @@
 package org.example.makispl.makispl;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import io.papermc.paper.command.brigadier.Commands;
-//import io.papermc.paper.command.brigadier.CommandSourceStack;
-//import io.papermc.paper.event.player.AsyncChatEvent;
-//import io.papermc.paper.event.player.PlayerArmSwingEvent;
-//import io.papermc.paper.plugin.bootstrap.BootstrapContext;
-//import io.papermc.paper.plugin.bootstrap.PluginBootstrap;
-//import io.papermc.paper.plugin.lifecycle.event.LifecycleEvent;
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-//import net.dv8tion.jda.api.Permission;
-//import net.dv8tion.jda.api.components.actionrow.ActionRow;
-//import net.dv8tion.jda.api.components.buttons.Button;
-//import net.dv8tion.jda.api.entities.Member;
-//import net.dv8tion.jda.api.entities.User;
-//import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-//import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-//import net.dv8tion.jda.api.interactions.IntegrationType;
-//import net.dv8tion.jda.api.interactions.InteractionContextType;
-//import net.dv8tion.jda.api.interactions.InteractionHook;
-//import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
-//import net.dv8tion.jda.api.interactions.commands.OptionMapping;
-//import net.dv8tion.jda.api.interactions.commands.build.Commands;
-//import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.dv8tion.jda.api.requests.GatewayIntent;
-//import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
-//import org.bukkit.ChatColor;
-//import org.bukkit.Material;
-//import org.bukkit.enchantments.Enchantment;
-//import org.bukkit.entity.Entity;
-//import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
-//import org.bukkit.event.block.Action;
-//import org.bukkit.event.block.BlockBreakEvent;
-//import org.bukkit.event.entity.EntityDamageByEntityEvent;
-//import org.bukkit.event.entity.FoodLevelChangeEvent;
-//import org.bukkit.event.entity.PlayerDeathEvent;
-//import org.bukkit.event.player.PlayerInteractEvent;
-//import org.bukkit.event.player.PlayerItemConsumeEvent;
-//import org.bukkit.event.player.PlayerChatEvent;
-//import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
-//import org.bukkit.inventory.ItemStack;
-//import java.util.Collection;
-//import java.util.Collections;
 import java.io.*;
 import java.util.EnumSet;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
-
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.example.makispl.makispl.listeners.BlockBreakListener;
 import org.example.makispl.makispl.listeners.PlayerDeathListener;
 import org.example.makispl.makispl.listeners.PlayerJoinListener;
-
 import static io.papermc.paper.command.brigadier.Commands.argument;
 import static io.papermc.paper.command.brigadier.Commands.literal;
-
-import java.util.Scanner;
 
 public final class Makispl extends JavaPlugin {
     public CustomCrafting customRecipe = new CustomCrafting(this);
     private Makispl plugin;
     private JDA jda;
-    Map<String, String> configMap = new HashMap<>();
+//    Map<String, String> configMap = new HashMap<>();
     @Override
     public void onEnable() {
-    File configFile = new File("makis.yml");
-    String defaultVal = "BOT_TOKEN=PUT_TOKEN_HERE\n" +
-                        "TARGET_CHANNEL_ID=PUT_CHANNEL";
-    if (!configFile.exists()) {
-        try {
-            configFile.createNewFile();
-            FileWriter myWriter = new FileWriter(configFile);
-            myWriter.write(defaultVal);
-            myWriter.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    try (Scanner myReader = new Scanner(configFile)) {
-        while (myReader.hasNextLine()) {
-            String data= myReader.nextLine();
-            if (data.contains("=")) {
-                int index = data.indexOf("=");
-                String part1 = data.substring(index - 1);
-                String part2 = data.substring(index + 1);
-                configMap.put(part1, part2);
-            }
-        }
-    } catch (FileNotFoundException e) {
-        e.printStackTrace();
-    }
-    boolean ENABLE_DC_BOT= Boolean.parseBoolean(configMap.getOrDefault("ENABLE_DC_BOT", "false"));
-    final long targetChannelId = Long.parseLong(configMap.getOrDefault("TARGET_CHANNEL_ID", "1"));
+        saveDefaultConfig();
+    boolean ENABLE_DC_BOT= Boolean.parseBoolean(getConfig().getString("ENABLE_DC_BOT"));
+    final long targetChannelId = Long.parseLong(getConfig().getString("TARGET_CHANNEL_ID"));
         // 1446543469723259104L
     if (ENABLE_DC_BOT) {
         try {
             // 1. Login to discord
             EnumSet<GatewayIntent> intents = EnumSet.noneOf(GatewayIntent.class);
-            String BOT_TOKEN = configMap.getOrDefault("BOT_TOKEN", "LOL");
+            String BOT_TOKEN = getConfig().getString("BOT_TOKEN");
             // MTQ0NjU0MzU3MzkwNTQ0MDc5OQ.GWDWFL._puyxFdNogj3wJSIATVcfGQ0F9g9HQZFo1pZUY
             jda = JDABuilder.createLight(BOT_TOKEN, intents)
                     .addEventListeners(new DiscordCommandListener(this))
@@ -119,7 +49,7 @@ public final class Makispl extends JavaPlugin {
             ).queue();
             getLogger().info("Discord bot is online!!!");
         } catch (Exception e) {
-            getLogger().severe("Could not login to dicsord");
+            getLogger().severe("Could not login to discord");
             e.printStackTrace();
         }
         plugin = this;
